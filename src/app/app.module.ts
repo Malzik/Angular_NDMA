6c0fa53283ed5511e10431dc9c9906e85b8ea3e0
Alexis <alexis.heroin@epsi.fr>
2019-12-20 12:07:33 +0100

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterComponent } from './router.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+    { path: '', component: AppComponent},
+    { path: '**', component: AppComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RouterComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
