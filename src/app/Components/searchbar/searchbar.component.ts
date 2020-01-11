944cb5e3fdcd6d8bb02f65d91d8d54945ec2fb78
Alexis <alexis.heroin@epsi.fr>
2020-01-11 14:59:29 +0100

    init

---
+++ src/app/Components/searchbar/searchbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbar',
+  templateUrl: './searchbar.component.html',
+  styleUrls: ['./searchbar.component.scss']
+})
+export class SearchbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+    show_filter() {
+
+    }
+}
