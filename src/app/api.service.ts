dc0fa53d658b2465ddffcbd7ebe89cd7d1e7996a
Senniinn <39193668+Senniinn@users.noreply.github.com>
2020-01-11 17:36:03 +0100

    il arrive pas a envoyer ses données

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,19 +10,19 @@ export class ApiService {
 
     private key = '20b0535705c24954ccceac6e0ea0c3fa';
     private link = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=';
-    private tags = '&tags=';
-    private min_upload_date = '&min_upload_date=';
-    private max_upload_date = '&max_upload_date=';
-    private safe_search = '&safe_search=';
+    private tags;
+    private minUploadDate = '';
+    private maxUploadDate = '';
+    private nsfw = '';
     private end = '&format=json&nojsoncallback=1';
 
     constructor(private http: HttpClient) { }
 
     getPhotos(datas): Observable<Photo> {
-        this.tags += datas.search;
-        this.min_upload_date += '';
-        this.max_upload_date += '';
-        this.safe_search += '';
-        return this.http.get<Photo>(this.link + this.key + this.tags + this.min_upload_date + this.max_upload_date + this.safe_search + this.end);
+        this.tags = '&tags=' + datas.search;
+        this.minUploadDate += '&min_upload_date=' + datas.date_min;
+        this.maxUploadDate += '&max_upload_date=' + datas.date_max;
+        this.nsfw += '&safe_search=' + datas.safe_search;
+        return this.http.get<Photo>(this.link + this.key + this.tags + this.minUploadDate + this.maxUploadDate + this.nsfw + this.end);
     }
 }
