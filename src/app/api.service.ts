cdcab511d6ffe5d63cf750f63188d9ab4f2b8f94
Senniinn <39193668+Senniinn@users.noreply.github.com>
2020-01-11 16:07:25 +0100

    il arrive pas a envoyer ses données

---
+++ src/app/api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Photo} from './photo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+    private key = '20b0535705c24954ccceac6e0ea0c3fa';
+    private link = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=';
+    private tags = '&tags=';
+    private min_upload_date = '&min_upload_date=';
+    private max_upload_date = '&max_upload_date=';
+    private safe_search = '&safe_search=';
+
+    constructor(private http: HttpClient) { }
+
+    getPhotos(datas): Observable<Photo> {
+        this.tags += datas.search;
+        return this.http.get<Photo>(this.link + this.key + this.tags);
+    }
+}
